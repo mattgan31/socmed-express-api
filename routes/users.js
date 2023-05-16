9400bdc7267414f5ce58ed72a87f6c8f5eca7668
Rahmat Alamsyah <57090921+mattgan31@users.noreply.github.com>
2023-05-16 18:45:52 +0700

    change queries.js to controllers structure

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const query = require('./../queries');
+const usersControllers = require('../controllers/userControllers');
 
 /* GET users listing. */
-router.get('/', query.getUsers);
-router.post('/', query.createUser);
-router.get('/:id', query.getUserById);
-router.put('/:id', query.updateUser);
+router.get('/', usersControllers.getUsers);
+router.post('/', usersControllers.createUser);
+router.get('/:id', usersControllers.getUserById);
+router.put('/:id', usersControllers.updateUser);
 
 module.exports = router;
