ba1703496c9f23ca95f328e952c5791cc94c4930
mattgan31 <alamsyahr00@gmail.com>
2023-07-23 15:48:11 +0700

    Update routing file for register & login

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,8 @@ const usersControllers = require('../controllers/userControllers');
 const verifyToken = require('../middleware/verifyToken');
 
 /* GET users listing. */
+router.post('/register', usersControllers.createUser);
+router.post('/login', usersControllers.userLogin);
 router.get('/profile', verifyToken, usersControllers.getUserByAuth);
 router.get('/', verifyToken, usersControllers.getUsers);
 router.get('/:id', verifyToken, usersControllers.getUserById);
